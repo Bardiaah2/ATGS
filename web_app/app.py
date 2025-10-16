1dbb41f33da8c99ad4c0c7093ac0d854a3047de3
Kyran <kyran@dyn-10-108-109-5.wless.uoregon.edu>
2025-10-16 12:09:04 -0700

    Basic Flask app; added to requirements

--- web_app/app.py
+++ web_app/app.py
@@ -3,4 +3,13 @@ Web application for University of Oregon
 student/advisor ticketing system and graduation planner
 '''
 
-import Flask
+from flask import Flask 
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello from ATGS!"
+
+if __name__ == '__main__':
+    app.run(debug=True)
